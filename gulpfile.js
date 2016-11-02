21128dfeb1e940bd7d0ee756d26adf31b7dbfb41
xushilong <xushilong@dyrs.com.cn>
2016-11-02 16:51:00 +0800

    增加项目脚手架

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+//依赖插件
+var gulp = require('gulp'); //gulp
+var rename = require('gulp-rename'); //重命名
+var uglify = require('gulp-uglify'); //js混淆
+var concat = require('gulp-concat'); //合并
+var sass = require('gulp-sass'); // sass 编译
+var sourcemaps = require('gulp-sourcemaps');//sass map
+var jshint = require('gulp-jshint'); //js代码校验
+
+//判断开发环境，默认正式环境
+var dev = process.argv.splice(3)[0]==='-dev';
+
+
+// <script src="/node_modules/angular/angular.min.js"></script>
+// <script src="/node_modules/angular-animate/angular-animate.min.js"></script>
+// <script src="/node_modules/angular-ui-router/release/angular-ui-router.min.js"></script>
+// <script src="/node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"></script>
+// <script src="/node_modules/angular-sanitize/angular-sanitize.min.js"></script>
+// <script src="/static/js/router.js"></script>
+
+// 文件路径
+var style=['src/sass/app.scss'];
+var script=['node_modules/angular/angular.js','node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js','node_modules/angular-animate/angular-animate.js','node_modules/angular-sanitize/angular-sanitize.js','node_modules/angular-ui-router/release/angular-ui-router.js','src/js/router.js'];
+
+// sass编译
+gulp.task('sass', function() {
+	gulp.src(style)
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle:dev?'nested':'compressed' //outputStyle Type: String Default: nested Values: nested, expanded, compact, compressed
+		}).on('error', sass.logError))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('static/css/'))
+});
+
+
+//js合并 混淆
+gulp.task('jsmin', () =>{
+		var to ='static/js/',name='app.js';
+		if(dev){
+			gulp.src(script).pipe(concat(name)).pipe(rename({ suffix: '.min' })).pipe(gulp.dest(to));
+		}else{
+			gulp.src(script).pipe(concat(name)).pipe(rename({ suffix: '.min' })).pipe(uglify()).pipe(gulp.dest(to));
+		}
+});
+
+//js校验
+gulp.task('jshint', function() {
+	return gulp.src(['src/js/router.js','gulpfile.js'])
+		.pipe(jshint({eqeqeq:false}))
+		.pipe(jshint.reporter('default')) // 对代码进行报错提示
+});
+
+
+//自动监听
+gulp.task('watch', function() {
+	gulp.watch(style, ['sass']);
+	gulp.watch(script, ['jshint','jsmin']);
+});
+
+
+//help
+gulp.task('default', function() {
+	console.log(' | 老徐的前端自动化部署工具');
+	console.log(' | blog：//www.loveqiao.com');
+	console.log(' | git ：//github.com/waihaolaoxu/gulp.git');
+});
---
+++ src/js/router.js
@@ -0,0 +1,179 @@
+/**
+ * 公用控制器
+ */
+var app = angular.module("app", ["ui.router", "ui.bootstrap", 'ngAnimate', 'ngSanitize']);
+
+//首页
+app.controller("ctrl", ['$scope', function($scope) {
+    $scope.menuShow = false;
+    $scope.toggleMenu = function() {
+        $scope.menuShow = !$scope.menuShow;
+    };
+}]);
+
+//表单页面
+app.controller("form", ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
+    var bitian = ['name', 'select', 'sex'];
+    $scope.user = {}
+    $scope.submit = function() {
+        bitian.forEach(function(e) {
+            if (!$scope.user[e]) {
+                $scope.alerts.push({
+                    msg: e + '不能为空'
+                });
+            }
+        })
+    }
+}]);
+
+//列表页面
+app.controller("demoList", ['$scope', '$http', '$uibModal', '$log', function($scope, $http, $uibModal, $log) {
+    //分页
+    $scope.page = {
+        maxSize: 4, //最大可见分页
+        totalItems: 100, //总条数
+        currentPage: 1, //当前页码
+        itemsPerPage: 10, //每页条数
+        pageChange: function() {
+            getData($scope.page.currentPage);
+        }
+    }
+
+    function getData(n) {
+        $scope.loadingShow = true;
+        $http.get("http://m.jiajuol.com/partner/weixin/subject/subject_list.php?page=" + n).success(function(response) {
+            $scope.data = response.data;
+            $scope.tableShow = true;
+            $scope.loadingShow = false;
+        });
+    }
+    getData(1);
+
+    //删除确认模态框
+    $scope.del = function(data) {
+        var modalInstance = $uibModal.open({
+            templateUrl: '/template/modal.html',
+            controller: 'delDate',
+            backdrop: "static", //控制背景状态
+            // size: 'sm',
+            resolve: {
+                item: function() {
+                    return data;
+                }
+            }
+        });
+        modalInstance.result.then(function(data) {
+            $log.info(data);
+        }, function(data) {
+            $log.info(data);
+        });
+    }
+}]);
+app.controller('delDate', ['$scope', '$uibModalInstance', 'item', function($scope, $uibModalInstance, item) {
+    $scope.title = "系统提示";
+    $scope.content = "确定要删除 <span class='text-danger'>" + item.subject + "</span>？";
+    $scope.ok = function() {
+        $uibModalInstance.close('yes');
+    };
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+}]);
+
+
+
+/**
+ * 路由配置
+ */
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    //使用HTML5格式
+    // $locationProvider.html5Mode(true);
+
+    //如果没有满足的路由条件默认跳转主页
+    $urlRouterProvider.otherwise("/home");
+
+    //重定向
+    $urlRouterProvider.when("/demo", "/demo/tab1");
+
+    // 首页
+    $stateProvider.state("main", {
+        url: "/home",
+        template: "<h1>欢迎登录CRM管理系统</h1>"
+    })
+
+    // demo页面
+    .state("demo", {
+            url: "/demo",
+            templateUrl: "/view/demo/tab.html"
+        })
+        .state("demo.tab1", {
+            url: "/tab1",
+            templateUrl: "/view/demo/tab1/list.html",
+            controller: "demoList"
+        })
+        .state("demo.tab2", {
+            url: "/tab2",
+            templateUrl: "/view/demo/tab2/list.html",
+            controller: "demoList"
+        })
+        .state("demo_tab1_edit", {
+            url: "/demo/tab1/:id",
+            templateUrl: "/view/demo/tab1/edit.html",
+            controller: ['$stateParams', '$scope', function($stateParams, $scope) {
+                // alert($stateParams.id)
+                $scope.edit = true;
+                $scope.title = "tab1编辑"
+            }]
+        })
+        .state("demo_tab1_search", {
+            url: "/demo/tab1_search?id&type",
+            templateUrl: "/view/demo/tab1/list.html",
+            controller: ['$stateParams', '$scope', function($stateParams, $scope, $http) {
+                // alert($stateParams.id+'\n'+$stateParams.type)
+                $scope.search = true;
+                $scope.title = "tab1查询结果";
+            }]
+        })
+        .state("demo_tab2_edit", {
+            url: "/demo/tab2/:id",
+            templateUrl: "/view/demo/tab2/edit.html",
+            controller: ['$stateParams', '$scope', function($stateParams, $scope) {
+                // alert($stateParams.id)
+                $scope.edit = true;
+                $scope.title = "tab2编辑";
+            }]
+        })
+
+    // 提示页面
+    .state("tip", {
+        url: "/tip/:msg",
+        templateUrl: "/view/tip.html",
+        controller: ['$stateParams', '$scope', function($stateParams, $scope) {
+            $scope.msg = $stateParams.msg
+        }]
+    });
+}]);
+
+// 初始化运行
+app.run(['$rootScope', '$location', function($rootScope, $location) {
+    //监听路由变化，暂时无用
+    $rootScope.$on('$locationChangeSuccess', function(event, newUrl, oldUrl) {
+        // alert(newUrl)
+    });
+
+    //标记当前页面焦点
+    $rootScope.isActive = function(route) {
+        var flag = null;
+        if (typeof route === 'string') {
+            flag = ($location.path().indexOf(route) >= 0);
+        } else {
+            route.forEach(function(d) {
+                if ($location.path().indexOf(d) >= 0) {
+                    flag = true;
+                    return false;
+                }
+            })
+        }
+        return flag;
+    }
+}])
